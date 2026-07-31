acff99f0f2f6ad9cffdab6b9405dc973ceeb9a69
hashio.y <251zs03753@student.zen.ac.jp>
2026-07-31 18:47:37 +0900

    feat: add omikuji rogic

--- 01_omikuji/main.js
+++ 01_omikuji/main.js
@@ -1,1 +1,10 @@
 let omikuji = ["大吉", "中吉", "吉", "小吉", "凶", "大凶", "もう一回"];
+let randomIndex = Math.floor(Math.random() * omikuji.length);
+let result = omikuji[randomIndex];
+
+
+if (result === omikuji[6]) {
+  console.log("もう一回引けるよ！ボタンを押してね！")
+} else {
+  console.log(result);
+}
