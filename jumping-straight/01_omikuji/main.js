a7c02e740286f491a2a63c16524c02c70570fde5
hashio.y <251zs03753@student.zen.ac.jp>
2026-08-04 18:44:48 +0900

    feat: add function

--- jumping-straight/01_omikuji/main.js
+++ jumping-straight/01_omikuji/main.js
@@ -1,14 +1,15 @@
 const omikuji = ["大吉", "中吉", "吉", "小吉", "凶", "大凶", "もう一回"];
-const randomIndex = Math.floor(Math.random() * omikuji.length);
-let result = omikuji[randomIndex];
 
-function OmikujiPlay() {
+function omikujiPlay() {
+  const randomIndex = Math.floor(Math.random() * omikuji.length);
+const result = omikuji[randomIndex];
   if (result === omikuji[6]) {
   console.log("もう一回引けるよ！ボタンを押してね！");
     console.log("もう一回引く");
+    omikujiPlay();
 } else {
   console.log(result);
 }
 }
 
-OmikujiPlay()
+omikujiPlay();
