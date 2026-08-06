5f7d68ce791526a793dd921bde061e3272d17a82
hashio.y <251zs03753@student.zen.ac.jp>
2026-08-06 16:56:21 +0900

    fix: fix omikuji function

--- 00_book_js-jumping-straight/01_omikuji/main.js
+++ 00_book_js-jumping-straight/01_omikuji/main.js
@@ -1,24 +1,34 @@
 // おみくじの中身
-const omikuji = ["大吉", "中吉", "吉", "小吉", "凶", "大凶", "もう一回"];
+const omikuji = [
+  "大吉",
+  "中吉",
+  "吉",
+  "小吉",
+  "凶",
+  "大凶",
+  "もう一回"
+];
 
 // DOM操作のための変数
-const playBtn = document.querySelector('#play-btn');
-const resultDisplay = document.querySelector('.result-display');
-const oneMoreBtn = document.querySelector('.onemore-btn');
-const oneMoreResult = document.querySelector('.onemore-result-display');
-
-
+const playBtn = document.querySelector("#play-btn");
+const resultDisplay = document.querySelector(".result-display");
+const oneMoreBtn = document.querySelector(".onemore-btn");
 
 function omikujiPlay() {
   const randomIndex = Math.floor(Math.random() * omikuji.length);
   const result = omikuji[randomIndex];
-  if (result === omikuji[6]) {
-    resultDisplay.textContent = "もう一回引けるよ！下のボタンを押してね！";
+
+  if (result === "もう一回") {
+    resultDisplay.textContent =
+      "もう一回引けるよ！下のボタンを押してね！";
+
     oneMoreBtn.classList.add("is-show");
   } else {
     resultDisplay.textContent = result;
+
+    oneMoreBtn.classList.remove("is-show");
   }
-  oneMoreBtn.addEventListener("click", omikujiPlay);
 }
 
 playBtn.addEventListener("click", omikujiPlay);
+oneMoreBtn.addEventListener("click", omikujiPlay);
