40c57f27f21838bb5f4812fef4cbdbbaef0d0c57
hashio.y <251zs03753@student.zen.ac.jp>
2026-08-06 16:33:36 +0900

    feat: add base code of  omikuji-program

--- 00_book_js-jumping-straight/01_omikuji/main.js
+++ 00_book_js-jumping-straight/01_omikuji/main.js
@@ -1,14 +1,23 @@
+// おみくじの中身
 const omikuji = ["大吉", "中吉", "吉", "小吉", "凶", "大凶", "もう一回"];
 
+// DOM操作のための変数
+const playBtn = document.querySelector('#play-btn');
+const resultDisplay = document.querySelector('.result-display');
+const oneMoreBtn = document.querySelector('.onemore-btn');
+
+
+
 function omikujiPlay() {
+  
   const randomIndex = Math.floor(Math.random() * omikuji.length);
-const result = omikuji[randomIndex];
+  const result = omikuji[randomIndex];
   if (result === omikuji[6]) {
-  console.log("もう一回引けるよ！ボタンを押してね！");
-    console.log("もう一回引く");
+  resultDisplay.addEventListener("もう一回引けるよ！ボタンを押してね！");
+  playBtn.addEventListener('click', "もう一回引く");
     omikujiPlay();
 } else {
-  console.log(result);
+  resultDisplay.textContent = result;
 }
 }
 
