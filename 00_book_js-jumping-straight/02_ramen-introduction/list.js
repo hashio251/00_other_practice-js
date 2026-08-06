fbc830b8d7250200e3652d8110dce1f8d4b52c3c
hashio.y <251zs03753@student.zen.ac.jp>
2026-08-06 18:10:56 +0900

    chore: create js file about ramen-list

---
+++ 00_book_js-jumping-straight/02_ramen-introduction/list.js
@@ -0,0 +1,37 @@
+const misoRamen = [
+  {
+    name :  '田坂屋',
+    station : '池袋',
+    map : '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.822123912049!2d139.7068061!3d35.7305929!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188d88ebac9175%3A0xb96225c82b281a5!2z5ZGz5ZmM6bq65YemIOeUsOWdguWxiw!5e0!3m2!1sja!2sjp!4v1786006701500!5m2!1sja!2sjp" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>'
+  },
+  {
+    name : '',
+    station : '',
+    map : ''
+  },
+  {
+    name : '',
+    station : '',
+    map : ''
+  },
+  {
+    name : '',
+    station : '',
+    map : ''
+  },
+  {
+    name : '',
+    station : '',
+    map : ''
+  },
+  {
+    name : '',
+    station : '',
+    map : ''
+  },
+  {
+    name : '',
+    station : '',
+    map : ''
+  }
+];
---
+++ 00_book_js-jumping-straight/02_ramen-introduction/main.js
@@ -0,0 +1,1 @@
+import * as ramen from "./list.js";
