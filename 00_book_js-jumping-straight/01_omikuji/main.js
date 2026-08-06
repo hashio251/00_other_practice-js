c60acbd6c5c33609488abcb75de8653b22c1237c
hashio.y <251zs03753@student.zen.ac.jp>
2026-08-06 16:54:41 +0900

    feat: add detail of omikujiplay function

--- 00_book_js-jumping-straight/01_omikuji/main.js
+++ 00_book_js-jumping-straight/01_omikuji/main.js
@@ -5,20 +5,20 @@ const omikuji = ["大吉", "中吉", "吉", "小吉", "凶", "大凶", "もう�
 const playBtn = document.querySelector('#play-btn');
 const resultDisplay = document.querySelector('.result-display');
 const oneMoreBtn = document.querySelector('.onemore-btn');
+const oneMoreResult = document.querySelector('.onemore-result-display');
 
 
 
 function omikujiPlay() {
-  
   const randomIndex = Math.floor(Math.random() * omikuji.length);
   const result = omikuji[randomIndex];
   if (result === omikuji[6]) {
-  resultDisplay.addEventListener("もう一回引けるよ！ボタンを押してね！");
-  playBtn.addEventListener('click', "もう一回引く");
-    omikujiPlay();
-} else {
-  resultDisplay.textContent = result;
-}
+    resultDisplay.textContent = "もう一回引けるよ！下のボタンを押してね！";
+    oneMoreBtn.classList.add("is-show");
+  } else {
+    resultDisplay.textContent = result;
+  }
+  oneMoreBtn.addEventListener("click", omikujiPlay);
 }
 
-omikujiPlay();
+playBtn.addEventListener("click", omikujiPlay);
