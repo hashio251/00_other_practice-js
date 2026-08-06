832c1faf603616194a8e6be8121ef5416818e463
hashio.y <251zs03753@student.zen.ac.jp>
2026-08-06 18:22:11 +0900

    feat: add miso ra-men list

--- 00_book_js-jumping-straight/02_ramen-introduction/list.js
+++ 00_book_js-jumping-straight/02_ramen-introduction/list.js
@@ -1,37 +1,40 @@
 const misoRamen = [
   {
-    name :  '田坂屋',
+    name :  '味噌麺処 田坂屋',
     station : '池袋',
     map : '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.822123912049!2d139.7068061!3d35.7305929!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188d88ebac9175%3A0xb96225c82b281a5!2z5ZGz5ZmM6bq65YemIOeUsOWdguWxiw!5e0!3m2!1sja!2sjp!4v1786006701500!5m2!1sja!2sjp" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>'
   },
   {
-    name : '',
-    station : '',
-    map : ''
+    name : '麺処 花田 池袋店',
+    station : '池袋',
+    map : '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.8751181624525!2d139.71105838885498!3d35.72928969999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188d68bc75857f%3A0xb1c187e8c053636!2z6bq65YemIOiKseeUsCDmsaDooovlupc!5e0!3m2!1sja!2sjp!4v1786007525629!5m2!1sja!2sjp" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>'
   },
   {
-    name : '',
-    station : '',
-    map : ''
+    name : '味噌麺処 花道庵 野方本店',
+    station : '野方',
+    map : '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d51843.681061507225!2d139.66081532936852!3d35.69595569066839!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018ed782e01653b%3A0xc104ee1754d8be60!2z5ZGz5ZmM6bq65YemIOiKsemBk-W6tQ!5e0!3m2!1sja!2sjp!4v1786007582854!5m2!1sja!2sjp" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>'
   },
   {
-    name : '',
-    station : '',
-    map : ''
+    name : 'あさひ町内会',
+    station : '板橋区役所前',
+    map : '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.0283628394814!2d139.70957751260724!3d35.7501076260709!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018937d7b559a15%3A0x98ec9e8c80aee944!2z44GC44GV44Gy55S65YaF5Lya!5e0!3m2!1sja!2sjp!4v1786007696889!5m2!1sja!2sjp" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>'
   },
   {
-    name : '',
-    station : '',
-    map : ''
+    name : '恵比寿 らぁ麺屋 つなぎ',
+    station : '恵比寿',
+    map : '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3242.1863298802978!2d139.7090999126049!3d35.64778063169449!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188b6a82064a21%3A0x5573393393611c43!2z5oG15q-U5a-_IOOCieOBgem6uuWxiyDjgaTjgarjgY4!5e0!3m2!1sja!2sjp!4v1786007811323!5m2!1sja!2sjp" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>'
   },
   {
-    name : '',
-    station : '',
-    map : ''
+    name : '濃厚背脂みそらーめん 桜田みそら',
+    station : '五反田',
+    map : '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6486.25439227108!2d139.71297905870364!3d35.62459068166231!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188b0f4bfde3d7%3A0x611c2387018e9686!2z5r-D5Y6a6IOM6ISC44G_44Gd44KJ44O844KB44KTIOahnOeUsOOBv-OBneOCiQ!5e0!3m2!1sja!2sjp!4v1786007892344!5m2!1sja!2sjp" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>'
   },
   {
-    name : '',
-    station : '',
-    map : ''
+    name : '九段下 中路',
+    station : '九段下',
+    map : '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.194195202959!2d139.74905491260606!3d35.69683852899994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188d60cf405369%3A0xee694fcd227e1e09!2z5Lmd5q615LiLIOS4rei3rw!5e0!3m2!1sja!2sjp!4v1786007994882!5m2!1sja!2sjp" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>'
   }
 ];
+
+
+
