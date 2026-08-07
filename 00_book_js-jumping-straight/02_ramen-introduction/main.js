9e2c39408ba279432266b7429c9559a565be1701
hashio.y <251zs03753@student.zen.ac.jp>
2026-08-07 17:13:43 +0900

    feat: add category in html

--- 00_book_js-jumping-straight/02_ramen-introduction/main.js
+++ 00_book_js-jumping-straight/02_ramen-introduction/main.js
@@ -1,1 +1,6 @@
 import * as ramen from "./list.js";
+
+const storeNameDisplay = document.querySelector('.store-name');
+const storeStationDisplay = document.querySelector('store-station');
+const storeMapDisplay = document.querySelector('#map-dispaly');
+
